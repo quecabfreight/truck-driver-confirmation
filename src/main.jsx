aaf29b36f11a5653d427104201a2ed1f2182ca72
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-10-20 19:06:13 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
+// Load our styles once
 import "./pages/verify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
