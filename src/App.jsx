7bbdec8c972627787352082e1870e266dbd22447
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-10-22 19:06:22 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
-import Hello from "./pages/Hello.jsx";
-import Verify from "./pages/Verify.jsx";
-import Login from "./pages/Login.jsx";
-import Register from "./pages/Register.jsx";
+import ErrorBoundary from "/src/components/ErrorBoundary.jsx";
+import Hello from "/src/pages/Hello.jsx";
+import Verify from "/src/pages/Verify.jsx";
+import Login from "/src/pages/Login.jsx";
+import Register from "/src/pages/Register.jsx";
