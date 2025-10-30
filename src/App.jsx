1409c29cabceadd2f00422ef90bddcf07cf08ed4
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-10-29 20:55:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// PAGES
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
@@ -9,16 +8,10 @@ import Join from "./pages/Join";
 function App() {
   return (
     <div className="app-shell min-h-screen w-full bg-black text-white">
-      {/* App-wide background + routing */}
       <Router>
         <Routes>
-          {/* Dashboard / landing */}
           <Route path="/" element={<Home />} />
-
-          {/* Broker/Shipper login */}
           <Route path="/login" element={<Login />} />
-
-          {/* Request access / authorization form */}
           <Route path="/join" element={<Join />} />
         </Routes>
       </Router>
