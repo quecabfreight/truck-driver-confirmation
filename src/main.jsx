66d08142bd025c7458943dcebe3db2c358d1394b
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-11-04 18:56:13 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
-import Join from './pages/Join.jsx'
-import DriverPing from './pages/DriverPing.jsx'
-import VerifyDriver from './pages/VerifyDriver.jsx'
-import './styles.css'
-import { applyInitialTheme } from './utils/theme.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-// set theme before anything renders
-applyInitialTheme()
-
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          {/* Smart Link routes */}
-          <Route path="/s/:token" element={<DriverPing />} />
-          <Route path="/verify/:token" element={<VerifyDriver />} />
-        </Route>
-      </Routes>
+      <App />
     </HashRouter>
   </React.StrictMode>
-)
+);
