4f2fb47a187c777c019f2545c5199db047aaddc1
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-11-07 09:19:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
-// Components
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
-// Pages
 import Home from "./pages/Home.jsx";
 import Login from "./pages/Login.jsx";
 import Join from "./pages/Join.jsx";
@@ -13,7 +11,7 @@ import VerifyDriver from "./pages/VerifyDriver.jsx";
 import DriverScreen from "./pages/DriverScreen.jsx";
 import SmartLink from "./pages/SmartLink.jsx";
 
-function App() {
+export default function App() {
   return (
     <HashRouter>
       <Header />
@@ -24,12 +22,9 @@ function App() {
         <Route path="/verify/:token" element={<VerifyDriver />} />
         <Route path="/s/:token" element={<DriverScreen />} />
         <Route path="/smart" element={<SmartLink />} />
-        {/* Catch-all fallback */}
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
     </HashRouter>
   );
 }
-
-export default App;
