558c6ca2d47083bef3daf1d26d2bac6d54ea2c3f
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-11-09 18:33:55 -0500

    Create build.js

---
+++ src/config/build.js
@@ -0,0 +1,3 @@
+// One place to stamp the build shown in the UI and bug reports.
+export const BUILD_ID = "2025.11.09-β1"; // update when you deploy new builds
+export const BUGS_EMAIL = "quecabadbs@gmail.com"; // where bug emails go
