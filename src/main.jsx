54ed147f0da9d72373c71948816187b4805593bb
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-11-09 19:20:15 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import App from "./App";
+import "./styles.css"; // global styles (big type, contrast, etc.)
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
