2cca75d7234dfd4be9eb51a8f50eff7d4f10e099
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-11-10 18:40:17 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,8 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="site-footer">
-      © QueCab AdbS™ 2025 • BETA • Build 2025.11.09-β1
+      © QueCab AdbS™ 2025 • BETA • Build 2025.11.10-β1
     </footer>
   );
 }
+
