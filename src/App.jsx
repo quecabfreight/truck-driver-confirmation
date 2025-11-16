75c75ea20a159dfb055ac8795617ec5e890f49da
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-11-16 18:03:18 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login"; // <- This is your new file
 
 function App() {
   return (
-    <div className="homepage">
-      <div className="nav-links">
-        <a href="/home">Home</a>
-        <a href="/request-access">Request Access</a>
-        <a href="/login">Login</a>
-        <a href="/check-in">Check In</a>
-        <a href="/about">About</a>
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* your other routes */}
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
