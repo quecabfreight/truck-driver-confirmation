3734cc157ddc7f4eb7b7842f40c33cf59ba75450
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-11-30 15:13:06 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Join from "./pages/Join.jsx";
 import Login from "./pages/Login.jsx";
 import VerifyDriver from "./pages/VerifyDriver.jsx";
 import ControlCenter from "./pages/ControlCenter.jsx";
+import HowItWorks from "./pages/HowItWorks.jsx";
 
 export default function App() {
   return (
@@ -15,8 +16,8 @@ export default function App() {
         {/* HOME */}
         <Route path="/" element={<Home />} />
 
-        {/* HOW IT WORKS – for now reuses Home */}
-        <Route path="/how-it-works" element={<Home />} />
+        {/* HOW IT WORKS */}
+        <Route path="/how-it-works" element={<HowItWorks />} />
 
         {/* REQUEST ACCESS */}
         <Route path="/join" element={<Join />} />
@@ -24,7 +25,7 @@ export default function App() {
         {/* LOGIN */}
         <Route path="/login" element={<Login />} />
 
-        {/* AdbS CONTROL CENTER */}
+        {/* CONTROL CENTER */}
         <Route path="/control-center" element={<ControlCenter />} />
 
         {/* TRUCK-DRIVER VERIFICATION */}
