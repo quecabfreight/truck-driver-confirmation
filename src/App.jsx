9c78b28a87bc100cd9b61f282fa4c12c8ac2733a
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-12-15 19:11:45 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import React from "react";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
+import Layout from "./components/Layout.jsx";
 
 import Home from "./pages/Home.jsx";
+import HowItWorks from "./pages/HowItWorks.jsx";
 import Join from "./pages/Join.jsx";
 import Login from "./pages/Login.jsx";
-import HowItWorks from "./pages/HowItWorks.jsx";
+import ControlCenter from "./pages/ControlCenter.jsx";
+import VerifyDriver from "./pages/VerifyDriver.jsx";
 
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/how-it-works" element={<HowItWorks />} />
+        {/* Pages that should have the standard site shell */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
+          <Route path="/join" element={<Join />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/control-center" element={<ControlCenter />} />
+          <Route path="*" element={<Home />} />
+        </Route>
 
-        {/* safety */}
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* Verify screen stays standalone (dock screen) */}
+        <Route path="/verify/:token" element={<VerifyDriver />} />
       </Routes>
     </HashRouter>
   );
