0fc63b6bb83ee01b0f2510a8789eeaec944dc45c
Truck/Driver Confirmation Phase I <quecabfreight@gmail.com>
2025-12-19 14:02:48 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
-// IMPORTANT: keep your styling alive
-import "./App.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
