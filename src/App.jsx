a2302a83141f9ed54faa7d20b8f1ebd21b4f42a9
Truck/Driver Confirmation Phase I <202360334+quecabfreight@users.noreply.github.com>
2026-03-22 12:29:24 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,15 @@ import Login from "./pages/Login.jsx";
 import Join from "./pages/Join.jsx";
 import HowItWorks from "./pages/HowItWorks.jsx";
 import ControlCenter from "./pages/ControlCenter.jsx";
-import Feedback from "./pages/Feedback.jsx";
-import Account from "./pages/Account.jsx";
-import Admin from "./pages/Admin.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ControlCenter />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/how-it-works" element={<HowItWorks />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/admin" element={<Admin />} />
         <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
